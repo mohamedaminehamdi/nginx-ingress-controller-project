6e5bbb0633b1a5d6d56c2ea03d132f1ae0c2f0dc
mohamedaminehamdi <aminehamdifcb@gmail.com>
2025-05-26 22:54:02 +0100

    add nginx.conf and docker-compose

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ const path = require('path');
 const app = express();
 const port = 3005;
 
+const appName = process.env.APP_NAME
 // Serve static files from assets folder
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 // Serve index.html for root route
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
-    console.log(`Request served by MY APP`);
+    console.log(`Request served by ${appName}`);
 });
 
 app.listen(port, () => {
-    console.log(`MY APP is listening on port ${port}`);
+    console.log(`${appName} is listening on port ${port}`);
 });
